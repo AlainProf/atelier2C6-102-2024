394c76d9671853b30a71edadf5f4d76d126a1a72
alainprof <amartel@cstj.qc.ca>
2024-01-24 15:49:13 -0500

    Commit initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace atelier2C6_102_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atelier du cours 420-2C6 (gr102) !");
+        }
+    }
+}
